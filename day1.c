f41926d943d032441ee1df1e7e6a1fd2cc32d459
genzonline <genzonlinew@gmail.com>
2024-12-01 18:04:16 +0400

    Part 2 of day 1

--- day1.c
+++ day1.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -10,19 +9,33 @@ typedef struct {
     int size;
 } Column;
 
-int fill_arrays(FILE *fp, Column *left, Column *right);
+typedef struct _tree_node {
+    int val;
+    int count;
+    struct _tree_node *left;
+    struct _tree_node *right;
+} TNode;
+
+int fill_arrays(FILE *fp, Column *left, Column *right, TNode **right_tree);
 int comp_int(int *a, int *b);
-Column *initialize_column();
+Column *initialize_column(void);
+TNode *initialize_tree(void);
 void free_column(Column *col);
-unsigned long calc_res(Column *left, Column *right, int len);
+void free_tree(TNode *tree);
+int get_count(TNode *tree, int val);
+TNode *add_val(TNode *tree, int val);
+unsigned long calc_dist(Column *left, Column *right, int len);
+unsigned long calc_sim(Column *left, TNode *right_tree, int len);
 void print_column(Column *col, int len);
+void print_tree(TNode *tree);
 
 int main(int argc, char *argv[]) {
     FILE *fp;
     int len;
     char *filename;
     Column *left, *right;
-    unsigned long distance;
+    unsigned long distance, similarity;
+    TNode *right_tree;
 
     if (argc == 1) {
         filename = FILENAME;
@@ -45,12 +58,13 @@ int main(int argc, char *argv[]) {
     left = initialize_column();
     printf("Initializing right column... ");
     right = initialize_column();
+    printf("Initializing right tree... ");
+    right_tree = initialize_tree();
     
     // scan file into two dynamic arrays
-    len = fill_arrays(fp, left, right);
+    len = fill_arrays(fp, left, right, &right_tree);
 
     // sort both arrays
-
     printf("Sorting arrays... ");
     qsort(left->values, len, sizeof(int), (__compar_fn_t) comp_int);
     /*print_column(left, len);*/
@@ -59,16 +73,20 @@ int main(int argc, char *argv[]) {
     printf("Success\n");
 
     // iterate over arrays and compute the result
-    distance = calc_res(left, right, len);
+    distance = calc_dist(left, right, len);
+    similarity = calc_sim(left, right_tree, len);
 
     printf("Freeing memory... ");
     free_column(left);
     free_column(right);
+    free_tree(right_tree);
     fclose(fp);
     printf("Success\n");
 
     printf("The distance between left and right lists is\n"
            "**************************** %lu ****************************\n", distance);
+    printf("The similarity score between left and right lists is\n"
+           "**************************** %lu ****************************\n", similarity);
     exit(0);
 }
 
@@ -98,6 +116,18 @@ void free_column(Column *col) {
     free(col);
 }
 
+TNode *initialize_tree(void) {
+    return NULL;
+}
+
+void free_tree(TNode *tree) {
+    if (tree == NULL)
+        return;
+    free_tree(tree->left);
+    free_tree(tree->right);
+    free(tree);
+}
+
 int comp_int(int *a, int *b) {
     if (*a < *b)
         return -1;
@@ -108,12 +138,13 @@ int comp_int(int *a, int *b) {
 }
 
 #define MAXLINE     100
-int add_numbers(Column *left, Column *right, char *left_p, char *right_p, int len);
+int add_numbers(Column *left, Column *right, TNode **right_tree, char *left_p, char *right_p, int len);
 
-int fill_arrays(FILE *fp, Column *left, Column *right) {
+int fill_arrays(FILE *fp, Column *left, Column *right, TNode **right_tree) {
     int len = 0;
     size_t maxline = MAXLINE;
     char *line = (char *)malloc(maxline);
+    char *left_num, *right_num;
 
     printf("Reading columns from input...\n");
 
@@ -129,22 +160,20 @@ int fill_arrays(FILE *fp, Column *left, Column *right) {
 
     printf("\tReading lines...\n");
     while (fgets(line, MAXLINE, fp)) {
-        char *left_num, *right_num;
-
         // relying on input a bit, but whatever
         for(left_num = right_num = strtok(line, " \t\n"); right_num != NULL; right_num = strtok(NULL, " \t\n"))
             if (strlen(right_num) != 0 && right_num != left_num)
                 break;
 
-        // Logging first 20 lines
-        if (len < 20) {
-            printf("\t\tLine %d:\n", len + 1);
-            printf("\t\t\tleft: %s\n\t\t\tright: %s\n", left_num, right_num);
-        } else if (len == 20) {
-            printf("\t\t..........\n");
-        }
+        /*// Logging first 20 lines*/
+        /*if (len < 20) {*/
+        /*    printf("\t\tLine %d:\n", len + 1);*/
+        /*    printf("\t\t\tleft: %s\n\t\t\tright: %s\n", left_num, right_num);*/
+        /*} else if (len == 20) {*/
+        /*    printf("\t\t..........\n");*/
+        /*}*/
 
-        len = add_numbers(left, right, left_num, right_num, len);
+        len = add_numbers(left, right, right_tree, left_num, right_num, len);
         if (len == -1) {
             free(line);
             free_column(left);
@@ -159,7 +188,9 @@ int fill_arrays(FILE *fp, Column *left, Column *right) {
     return len;
 }
 
-int add_numbers(Column *left, Column *right, char *left_p, char *right_p, int len) {
+int add_numbers(Column *left, Column *right, TNode **right_tree, char *left_p, char *right_p, int len) {
+    int right_value;
+
     if (len >= left->size) {
         printf("\t\tReallocating arrays... ");
         left->size *= 2;
@@ -177,16 +208,73 @@ int add_numbers(Column *left, Column *right, char *left_p, char *right_p, int le
 
     }
 
+
     left->values[len] = atoi(left_p);
-    right->values[len] = atoi(right_p);
+
+    right_value = atoi(right_p);
+    right->values[len] = right_value;
+    *right_tree = add_val(*right_tree, right_value);
 
     return len + 1;
 }
 
-unsigned long calc_res(Column *left, Column *right, int len) {
+int get_count(TNode *tree, int val) {
+    if (tree == NULL)
+        return 0;
+    else if (val < tree->val)
+        return get_count(tree->left, val);
+    else if (val > tree->val)
+        return get_count(tree->right, val);
+    else
+        return tree->count;
+}
+
+TNode *add_val(TNode *tree, int val) {
+    TNode *new_node;
+
+    if (tree == NULL) {
+        new_node = (TNode *)malloc(sizeof(TNode));
+        new_node->val = val;
+        new_node->count = 1;
+        new_node->left = NULL;
+        new_node->right = NULL;
+        return new_node;
+    } else if (val < tree->val) {
+        tree->left = add_val(tree->left, val);
+    } else if (val > tree->val) {
+        tree->right = add_val(tree->right, val);
+    } else {
+        tree->count++;
+    }
+    return tree;
+}
+
+unsigned long calc_sim(Column *left, TNode *right_tree, int len) {
+    int prev = -1;
+    int count = 0;
+    unsigned long sim = 0;
+
+    printf("Calculating distance... ");
+    for (int i = 0; i < len; i++) {
+        if (left->values[i] == prev) {
+            count++;
+        } else {
+            if (prev != -1)
+                sim += count * prev * get_count(right_tree, prev);
+            prev = left->values[i];
+            count = 1;
+        }
+    }
+
+    sim += count * prev * get_count(right_tree, prev);
+    printf("Success\n");
+    return sim;
+}
+
+unsigned long calc_dist(Column *left, Column *right, int len) {
     unsigned long res = 0;
 
-    printf("Aggregating results... ");
+    printf("Calculating distance... ");
     for (int i = 0; i < len; i++) {
         res += abs(left->values[i] - right->values[i]);
     }
