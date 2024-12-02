35f0a38229333a5cdb864b9457dba5e5db5690f4
genzonline <genzonlinew@gmail.com>
2024-12-02 15:51:29 +0400

    Part 2 of day 2

--- day2.c
+++ day2.c
@@ -62,40 +62,69 @@ int main(int argc, char *argv[]) {
 }
 
 int is_safe(char *line) {
-    int direction = 0;
-    int previous = -1, current;
-
     char *token;
+    int buf[10];
+    int direction;
+    int prev, cur;
+    int len, i;
+    int violations[2];
+    int violations_num = 0;
+    int option1, option2;
+
+    prev = -1;
+    len = 0;
     for (token = strtok(line, " \t\n"); token != NULL; token = strtok(NULL, " \t\n")) {
         if (isdigit(*token)) {
-            current = atoi(token);
-
-            // if previous is -1 then it's the first element and we just skip to the next
-            if (previous == -1) {
-                previous = current;
-                continue;
+            cur = atoi(token);
+            if (prev != -1) {
+                buf[len++] = cur - prev;
             }
-
-            // if previous is not -1 but direction is 0 then it's the second element
-            // define direction, check it's value and move on
-            if (direction == 0) {
-                direction = current - previous;
-                if (0 == direction || abs(direction) > 3)
-                    return 0;
-                previous = current;
-                continue;
-            }
-
-            // if this is == 0 then current == previous and we return 0
-            // if this is < 0 then the sign of direction differs from the sign of cur - prev
-            // and sequence is not monotonous
-            if ((current - previous) * direction <= 0 || abs(current - previous) > 3)
-                return 0;
-
-            previous = current;
+            prev = cur;
         }
     }
-    return 1;
+
+    /*printf("\t\tDifs: ");*/
+    /*for (i = 0; i < len; i++) */
+    /*    printf("%d, ", buf[i]);*/
+    /*printf("\n");*/
+
+
+    for (direction = -1; direction <= 1; direction += 2) {
+        violations_num = 0;
+        for (i = 0; i < len; i++) {
+            if (buf[i] * direction <= 0 || abs(buf[i]) > 3) {
+                if (violations_num == 2) {
+                    violations_num++;
+                    break;
+                }
+                violations[violations_num++] = i;
+            }
+        }
+
+        if (violations_num == 0)
+            return 1;
+
+        if (violations_num == 1) {
+            if (violations[0] == 0 || violations[0] == len-1)
+                return 1;
+            else {
+                option1 = buf[violations[0]] + buf[violations[0] + 1];
+                option2 = buf[violations[0]] + buf[violations[0] - 1];
+
+                if (option1 * direction > 0 && abs(option1) <= 3 ||
+                    option2 * direction > 0 && abs(option2) <= 3 )
+                    return 1;
+            }
+        } else if (violations_num == 2) {
+            if (abs(violations[0] - violations[1]) == 1) {
+                option1 = buf[violations[0]] + buf[violations[1]];
+                if (option1 * direction > 0 && abs(option1) <= 3)
+                    return 1;
+            }
+        }
+    }
+
+    return 0;
 }
 
 
