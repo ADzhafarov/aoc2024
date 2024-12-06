a2d27cd4aa917997cf60cbb2e3943b2d26cdbfbd
genzonline <genzonlinew@gmail.com>
2024-12-07 01:42:41 +0400

    Day 6 finished

--- day6.c
+++ day6.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define HEGITH      140
+#define HEIGHT      140
 #define WIDTH       140
 
 typedef struct _coords {
@@ -38,12 +38,20 @@ Coordinates sum_coord(Coordinates x, Coordinates y);
 int is_obstacle(Coordinates coord, char *rows[]);
 int is_outside(Coordinates coord, int height, int width);
 void print_coord_tree(TNode *visited);
-int check_for_loop_option(Coordinates current, Coordinates direction, char *rows[], int height, int width, Coordinates start);
+int check_for_loop_option(
+    Coordinates current,
+    TNode *visited,
+    Coordinates direction,
+    char *rows[],
+    int height,
+    int width,
+    Coordinates start
+);
 Coordinates step(Coordinates current_coord, Coordinates *direction, int height, int width, char *rows[], int *full_turn);
 
 
 int main(void) {
-    char *rows[HEGITH] = {NULL};
+    char *rows[HEIGHT] = {NULL};
     size_t maxrowlen = WIDTH;
     int i = 0, j, line_len;
     Coordinates current_coord, direction, next_coord, prev_coord, start;
@@ -56,7 +64,7 @@ int main(void) {
     int real_width, real_height;
 
     printf("Reading lines of input... ");
-    while (i < HEGITH && (line_len = getline(rows + i++, &maxrowlen, stdin)) > 0)
+    while (i < HEIGHT && (line_len = getline(rows + i++, &maxrowlen, stdin)) > 0)
         for (j = 0; j < line_len; j++) {
             symbol = rows[i-1][j];
             if (symbol == '<' || symbol == '^' || symbol == '>' || symbol == 'v' || symbol == 'V') {
@@ -85,11 +93,11 @@ int main(void) {
     prev_coord.j = -1;
     while (1) {
         visited = add_coordinate(visited, current_coord, &new);
-        printf("\tGuard at (%d, %d) [%s]\n", current_coord.i, current_coord.j, new ? "new" : "old");
+        /*printf("\tGuard at (%d, %d) [%s]\n", current_coord.i, current_coord.j, new ? "new" : "old");*/
         count_unique += new;
 
         printf("\t\tCheking for loop possibility... ");
-        loop_possible = check_for_loop_option(current_coord, direction, rows, real_height, real_width, start);
+        loop_possible = check_for_loop_option(current_coord, visited, direction, rows, real_height, real_width, start);
         loop_options += loop_possible;
         printf("%s", loop_possible ? "Possible\n" : "Impossible\n");
 
@@ -121,6 +129,11 @@ Coordinates step(Coordinates current_coord, Coordinates *direction, int height, 
             turn_direction(direction);
             next_coord = sum_coord(current_coord, *direction);
             *full_turn = 1;
+            if (is_obstacle(next_coord, rows)) {
+                turn_direction(direction);
+                next_coord = sum_coord(current_coord, *direction);
+                *full_turn = 0;
+            }
         } else
             *full_turn = 0;
     } else
@@ -130,41 +143,54 @@ Coordinates step(Coordinates current_coord, Coordinates *direction, int height, 
 
 }
 
-int check_for_loop_option(Coordinates current, Coordinates direction, char *rows[], int height, int width, Coordinates start) {
+int find_in_tree(TNode *visited, Coordinates coord);
+
+int check_for_loop_option(Coordinates current, TNode *visited, Coordinates direction, char *rows[], int height, int width, Coordinates start) {
     TNode *loop_visited = NULL;
     Coordinates new_obstacle = sum_coord(current, direction);
     Coordinates fast, slow;
     Coordinates fast_dir, slow_dir;
+    int full_turn, i;
+    int verbose = 0;
+
+    /*if (new_obstacle.i == 11 && new_obstacle.j == 12) {*/
+    /*    verbose = 1;*/
+    /*}*/
+
     fast_dir = slow_dir = direction;
     fast = slow = current;
-    int full_turn;
 
     if (is_outside(new_obstacle, height, width))
         return 0;
 
-    if (is_obstacle(new_obstacle, rows))
-        return 0;
+    if (is_obstacle(new_obstacle, rows)) {
+        turn_direction(&direction);
+        new_obstacle = sum_coord(current, direction);
+    }
 
     if (compare_coords(&new_obstacle, &start) == 0)
+        return 0;
+
+    if (find_in_tree(visited, new_obstacle))
         return 0;
 
     rows[new_obstacle.i][new_obstacle.j] = '#';
 
     while (1) {
         slow = step(slow, &slow_dir, height, width, rows, &full_turn);
-        fast = step(fast, &fast_dir, height, width, rows, &full_turn);
-        if (is_outside(fast, height, width)) {
-            rows[new_obstacle.i][new_obstacle.j] = '.';
-            return 0;
+        for (i = 0; i < 2; i++) {
+            fast = step(fast, &fast_dir, height, width, rows, &full_turn);
+            if (is_outside(fast, height, width)) {
+                rows[new_obstacle.i][new_obstacle.j] = '.';
+                return 0;
+            }
         }
-        fast = step(fast, &fast_dir, height, width, rows, &full_turn);
-        if (is_outside(fast, height, width)) {
-            rows[new_obstacle.i][new_obstacle.j] = '.';
-            return 0;
-        }
-        if (compare_coords(&slow, &fast) == 0) {
+        if (verbose)
+            printf("\t\t fast: (%d, %d), slow: (%d, %d)\n", fast.i, fast.j, slow.i, slow.j);
+        if (compare_coords(&slow, &fast) == 0 && compare_coords(&slow_dir, &fast_dir) == 0) {
             rows[new_obstacle.i][new_obstacle.j] = '.';
             printf(" (%d, %d) ", new_obstacle.i, new_obstacle.j);
+
             return 1;
         }
     }
@@ -199,6 +225,19 @@ TNode *add_coordinate(TNode *visited, Coordinates coord, int *new) {
     }
 
     return visited;
+}
+
+int find_in_tree(TNode *visited, Coordinates coord) {
+    int comp;
+
+    if (visited == NULL)
+        return 0;
+    else if ((comp = compare_coords(&visited->value, &coord)) < 0)
+        return find_in_tree(visited->left, coord);
+    else if (comp > 0)
+        return find_in_tree(visited->right, coord);
+    else
+        return 1;
 }
 
 void free_tree(TNode *tree) {
